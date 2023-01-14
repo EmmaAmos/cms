e6dd2cad917a3a7b7a022c9751356b6dfcba2c98
EmmaAmos <104381292+EmmaAmos@users.noreply.github.com>
2023-01-14 12:39:07 -0500

    old code

--- src/app/shared/contact-veiwable.model.ts
+++
@@ -1,4 +0,0 @@
-export class ContactVeiwable {    
-    constructor(public id: string, public name: string, public email: string, public phone: string, public imageUrl: string, public group: string ) {
-    }
-}
