0cd1eecefa234a838520c073ee182c0331fe00ca
EmmaAmos <104381292+EmmaAmos@users.noreply.github.com>
2023-01-17 15:02:35 -0500

    latest push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,9 @@ import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +18,15 @@ import { ContactDetailComponent } from './contacts/contact-detail/contact-detail
     HeaderComponent,
     ContactsComponent,
     ContactListComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    ContactItemComponent,
+    DocumentsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/documents/documents.model.ts
@@ -0,0 +1,13 @@
+export class Contact {
+    public id: string;
+    public name: string;
+    public description: string;
+    public url: string;
+    public children: string;
+ 
+    constructor(id: string, name: string, name: string, name: string, name: string, ) {
+        this.id = id;
+        this.name = name;
+
+    }
+}
